3f3d788df882d89a55b7cbab021bdb8a883d4bbd
John <jddhalogamer@gmail.com>
2021-06-14 18:21:51 -0500

    debugging game logic

--- main.js
+++ main.js
@@ -6,29 +6,24 @@ function computerPlay() {
     return compChoices[random];
 }
 
-const playerChoice = "rock";
-const computerChoice = computerPlay();
-
-    console.log(playerChoice);
-    console.log(computerChoice);
-
-
 function playRound( playerChoice, computerChoice) {
-    if( playerChoice === "rock" && computerChoice === "paper") {
-        return "You lose, paper covers rock!";
-    } else if (playerChoice === "rock" && computerChoice === "scissors") {
-        return "You win, rock smashes scissors!";
-    } else if (playerChoice === "scissors" && computerChoice === "rock") {
-        return "You lose, rock smashes scissors!";
-    } else if (playerChoice === "scissors" && computerChoice === "paper") {
-        return "You win, scissors cuts paper!";
-    } else if (playerChoice === "paper" && computerChoice === "rock") {
-        return "You lose, rock covers paper!";
-    } else if (playerChoice === "paper" && computerChoice === "scissors") {
-        return "You lose, scissors cuts paper!";
-    } else if (playerChoice === computerChoice) {
+
+    if (playerChoice === computerChoice) {
         return "Tie round.";
     }
+    if (playerChoice !== computerChoice) {
+        return "Oops!"
+    }
 }
+
+let playerChoice = "rock";
+let computerChoice = computerPlay().toLowerCase();
+
+
+console.log(playerChoice);
+console.log(computerChoice);
+
+
+
 
 console.log(playRound());
