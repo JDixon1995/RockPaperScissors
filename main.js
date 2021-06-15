e4ad315b8d5f2d0f1d6a3d42079edd660f5ca5d1
John <jddhalogamer@gmail.com>
2021-06-15 17:39:04 -0500

    finished building and testing game logic. Game plays from the console.

--- main.js
+++ main.js
@@ -10,13 +10,21 @@ function playRound( playerChoice, computerChoice) {
 
     if (playerChoice === computerChoice) {
         return "Tie round.";
-    }
-    if (playerChoice !== computerChoice) {
-        return "Oops!"
+    } else if ( playerChoice == "rock" && computerChoice == "scissors") {
+        return "You win! Rock smashes scissors."
+    } else if ( playerChoice == "rock" && computerChoice == "paper") {
+        return "You lose. Paper covers rock."
+    } else if ( playerChoice == "paper" && computerChoice == "scissors") {
+        return "You lose. Scissors cuts paper."
+    } else if ( playerChoice == "paper" && computerChoice == "rock") {
+        return "You win! Paper covers rock."
+    } else if ( playerChoice == "scissors" && computerChoice == "rock") {
+        return "You lose. Rock smashes scissors."
+    } else if ( playerChoice == "scissors" && computerChoice == "paper") {
+        return "You win! Scissors cuts paper." 
     }
 }
-
-let playerChoice = "rock";
+let playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
 let computerChoice = computerPlay().toLowerCase();
 
 
@@ -26,4 +34,4 @@ console.log(computerChoice);
 
 
 
-console.log(playRound());
+console.log(playRound(playerChoice, computerChoice));
